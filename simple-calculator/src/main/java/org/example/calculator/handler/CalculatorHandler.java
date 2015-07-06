82027cc0f0c11c99a6c77af71af2637646d14109
kumarp <pritikumar3@gmail.com>
2015-07-06 15:15:07 -0400

    Update CalculatorHandler.java

--- simple-calculator/src/main/java/org/example/calculator/handler/CalculatorHandler.java
+++ simple-calculator/src/main/java/org/example/calculator/handler/CalculatorHandler.java
@@ -2,23 +2,23 @@ package org.example.calculator.handler;
 
 public class CalculatorHandler {
 
-	public Float doAddition(Float operand1, Float operand2) {
-		Float result = operand1 + operand2;
+	public int doAddition(int operand1, int operand2) {
+		int result = operand1 + operand2;
 		return result;
 	}
 
-	public Float doSubtraction(Float operand1, Float operand2) {
-		Float result = operand1 - operand2;
+	public int doSubtraction(int operand1, int operand2) {
+		int result = operand1 - operand2;
 		return result;
 	}
 
-	public Float doMultiplication(Float operand1, Float operand2) {
-		Float result = operand1 * operand2;
+	public int doMultiplication(int operand1, int operand2) {
+		int result = operand1 * operand2;
 		return result;
 	}
 
-	public Float doDivision(Float operand1, Float operand2) {
-		Float result = operand1 / operand2;
+	public int doDivision(int operand1, int operand2) {
+		int result = operand1 / operand2;
 		return result;
 	}
 
