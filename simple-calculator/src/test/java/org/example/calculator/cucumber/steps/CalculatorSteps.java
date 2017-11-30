34c69fce47daf6482736ea4db84c0cd205538c81
mflaim1 <mflaim@redhat.com>
2017-11-30 11:42:02 -0500

    added step code

--- simple-calculator/src/test/java/org/example/calculator/cucumber/steps/CalculatorSteps.java
+++ simple-calculator/src/test/java/org/example/calculator/cucumber/steps/CalculatorSteps.java
@@ -22,4 +22,40 @@ public class CalculatorSteps {
 
 	// TODO: Insert Cucumber Step Definitions Here
 
+	@Given("^the following calculator input:$")
+	public void the_following_calculator_input(
+			DataTable calculatorInputDataTable) throws Throwable {
+		List<Map<String, String>> rows = calculatorInputDataTable.asMaps();
+		for (Map<String, String> row : rows) {
+			operand1 = Integer.parseInt(row.get("Operand 1"));
+			operand2 = Integer.parseInt(row.get("Operand 2"));
+		}
+	}
+
+	@When("^I do an? addition calculation$")
+	public void I_do_a_addition_calculation() throws Throwable {
+		result = calculatorHandler.doAddition(operand1, operand2);
+	}
+
+	@When("^I do a subtraction calculation$")
+	public void I_do_a_subtraction_calculation() throws Throwable {
+		result = calculatorHandler.doSubtraction(operand1, operand2);
+	}
+
+	@When("^I do a multiplication calculation$")
+	public void I_do_a_multiplication_calculation() throws Throwable {
+		result = calculatorHandler.doMultiplication(operand1, operand2);
+	}
+
+	@When("^I do a division calculation$")
+	public void I_do_a_division_calculation() throws Throwable {
+		result = calculatorHandler.doDivision(operand1, operand2);
+	}
+
+	@Then("^I expect the result (-?\\d+)$")
+	public void I_expect_the_result(int expectedResult) throws Throwable {
+		assertTrue(result == expectedResult);
+	}
+	
+
 }
